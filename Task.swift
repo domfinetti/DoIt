4d51157d3b29c360354c94fbf6ff42a840fd3b25
Dominick Dayton Finetti <dom.finetti93@gmail.com>
2016-10-16 01:52:28 -0500

    added plus & back buttons

---
+++ Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Dominick Dayton Finetti on 10/14/16.
+//  Copyright © 2016 Zappy Code. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
